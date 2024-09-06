8db7661da7fe3bbe4163d03a8e3b91afe583c190
Andrew <mr.lozonschi@mai.ru>
2024-09-06 16:36:05 +0300

    Adding command, git status, git log

--- src/GitCommand.java
+++ src/GitCommand.java
@@ -4,5 +4,9 @@ public class GitCommand {
         System.out.println("-m");
 //        to check actual uncommited change we can use next command
         System.out.println("git diff");
+        //        to check git status, unstaged and staged file we can use next command
+        System.out.println("git status");
+        //        To see all commits we can use next command
+        System.out.println("git log");
     }
 }
