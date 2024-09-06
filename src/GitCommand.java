620af9210113bd7c20b799ab2419c01481d85cc6
Andrew <mr.lozonschi@mai.ru>
2024-09-06 16:34:34 +0300

    adding command, -m, git diff, git status, git log

---
+++ src/GitCommand.java
@@ -0,0 +1,8 @@
+public class GitCommand {
+    public static void main(String[] args) {
+        //        add message(comment) to commit we can use next command
+        System.out.println("-m");
+//        to check actual uncommited change we can use next command
+        System.out.println("git diff");
+    }
+}
