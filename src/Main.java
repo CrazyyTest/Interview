f02e480c5138e8879f54dbc7b60b93441a9bf2ca
Andrew <mr.lozonschi@mai.ru>
2024-09-06 16:32:07 +0300

    adding command -m, git diff, git status, git log

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        //        add message(comment) to commit we can use next command
+        System.out.println("-m");
+//        to check actual uncommited change we can use next command
+        System.out.println("git diff");
+//        to check git status, unstaged and staged file we can use next command
+        System.out.println("git status");
+        //        To see all commits we can use next command
+        System.out.println("git log");
     }
 }
